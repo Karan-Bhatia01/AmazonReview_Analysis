2cd4c46753e8de3b0ff933d367eaf0c0217946c1
Karan-Bhatia01 <bhatiakaran168@gmail.com>
2025-04-04 00:29:29 +0530

    final commit

--- app.py
+++ app.py
@@ -3,30 +3,30 @@ import pickle
 
 app = Flask(__name__)
 
-# Load the trained Model and Vectorizer
+# Load trained Model and Vectorizer
 with open(r"models/sentiment_model.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 
 with open(r"models/tfidf_vectorizer.pkl", "rb") as vectorizer_file:
     vectorizer = pickle.load(vectorizer_file)
 
-
 @app.route('/')
 def home():
-    return render_template('predict.html')  # Load the HTML page
-
+    return render_template('predict.html', review="", sentiment="")  # Load UI
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        review = request.form['review']  # Get user input
+        review = request.form['review'].strip()  # Get user input
+        if not review:
+            return render_template('predict.html', review=review, sentiment="⚠️ Please enter a review!")
+
         transformed_review = vectorizer.transform([review])  # Transform input using TF-IDF
         prediction = model.predict(transformed_review)  # Predict sentiment
         
-        sentiment = "Positive 😊" if prediction[0] == 1 else "Negative 😡"
+        sentiment = "🟢 Positive 😊" if prediction[0] == 1 else "🔴 Negative 😡"
         
         return render_template('predict.html', review=review, sentiment=sentiment)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
